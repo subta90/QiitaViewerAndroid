0b8175eb1e94b535965d39f39b5cfe1b41f17c72
subta90 <the.bird.nobility298@gmail.com>
2020-05-09 17:56:27 +0900

    Display Dummy Data.

---
+++ app/src/main/java/com/example/qiitaviewerandroid/ArticleItemViewHolder.kt
@@ -0,0 +1,46 @@
+package com.example.qiitaviewerandroid
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class ArticleAdapter: RecyclerView.Adapter<ArticleAdapter.ArticleItemViewHolder>() {
+
+    var articles = listOf<String>()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArticleItemViewHolder {
+        return ArticleItemViewHolder.from(parent)
+    }
+
+    override fun getItemCount() = articles.size
+
+    override fun onBindViewHolder(holder: ArticleItemViewHolder, position: Int) {
+        val article = articles[position]
+        holder.bind(article)
+    }
+
+    class ArticleItemViewHolder(val view: View): RecyclerView.ViewHolder(view) {
+        companion object {
+            fun from(parent: ViewGroup): ArticleItemViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val articleTextView = layoutInflater.inflate(R.layout.article_item_view, parent, false) as TextView
+                return ArticleItemViewHolder(articleTextView)
+            }
+        }
+
+        private val textView: TextView = view.findViewById(R.id.article_item_title)
+
+        fun bind(item: String) {
+            textView.text = item
+        }
+
+    }
+
+}
+
--- app/src/main/java/com/example/qiitaviewerandroid/ArticleListFragment.kt
+++ app/src/main/java/com/example/qiitaviewerandroid/ArticleListFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.DataBindingUtil.inflate
+import com.example.qiitaviewerandroid.databinding.FragmentArticleListBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,6 +24,8 @@ class ArticleListFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
+    private val dummyData = listOf<String>("hoge", "fuga", "moge")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -30,11 +35,15 @@ class ArticleListFragment : Fragment() {
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_article_list, container, false)
+        val binding: FragmentArticleListBinding = inflate(inflater, R.layout.fragment_article_list, container, false)
+        val adapter = ArticleAdapter()
+        adapter.articles = dummyData
+        binding.articleListRecyclerview.adapter = adapter
+        return binding.root
     }
 
     companion object {
