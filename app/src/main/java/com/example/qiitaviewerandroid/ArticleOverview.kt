2417130681741b490d819a711790e1c53e5b8955
subta90 <the.bird.nobility298@gmail.com>
2020-05-23 20:11:49 +0900

    Remove disuse import

--- app/src/main/java/com/example/qiitaviewerandroid/ArticleOverview.kt
+++ app/src/main/java/com/example/qiitaviewerandroid/ArticleOverview.kt
@@ -2,7 +2,6 @@ package com.example.qiitaviewerandroid
 
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
-import kotlinx.android.parcel.Parcelize
 
 @JsonClass(generateAdapter = true)
 data class ArticleOverview(
