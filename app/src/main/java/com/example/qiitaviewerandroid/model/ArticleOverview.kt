893f893caf9ec3355d813d538d2ef856736039eb
subta90 <the.bird.nobility298@gmail.com>
2020-09-13 17:35:36 +0900

    Adapt @Parcel to ArticleOverview data class.

--- app/src/main/java/com/example/qiitaviewerandroid/model/ArticleOverview.kt
+++ app/src/main/java/com/example/qiitaviewerandroid/model/ArticleOverview.kt
@@ -2,23 +2,29 @@ package com.example.qiitaviewerandroid.model
 
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
+import org.parceler.Parcel
+import org.parceler.ParcelConstructor
+import org.parceler.ParcelProperty
 
 @JsonClass(generateAdapter = true)
-data class ArticleOverview(
-    val id: String,
-    val title: String,
-    val user: User,
-    @Json(name = "created_at") val createdAt: String,
-    val tags: List<Tag>
+@Parcel
+data class ArticleOverview @ParcelConstructor constructor(
+    @ParcelProperty("id") val id: String,
+    @ParcelProperty("title") val title: String,
+    @ParcelProperty("user") val user: User,
+    @Json(name = "created_at") @ParcelProperty("createdAt") val createdAt: String,
+    @ParcelProperty("tags") val tags: List<Tag>
 ) {
 
     @JsonClass(generateAdapter = true)
-    data class User(
-        val id: String
+    @Parcel
+    data class User @ParcelConstructor constructor(
+        @ParcelProperty("id") val id: String
     )
 
     @JsonClass(generateAdapter = true)
-    data class Tag(
-        val name: String
+    @Parcel
+    data class Tag @ParcelConstructor constructor(
+        @ParcelProperty("name") val name: String
     )
 }
