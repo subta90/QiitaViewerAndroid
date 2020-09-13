738e12c9772092445c16cc119e7edb1fbace9a33
subta90 <the.bird.nobility298@gmail.com>
2020-09-13 18:14:31 +0900

    Add safeargs to dependencies

--- app/src/main/java/com/example/qiitaviewerandroid/model/ArticleOverview.kt
+++ app/src/main/java/com/example/qiitaviewerandroid/model/ArticleOverview.kt
@@ -1,30 +1,31 @@
 package com.example.qiitaviewerandroid.model
 
+import android.os.Parcelable
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
-import org.parceler.Parcel
+import kotlinx.android.parcel.Parcelize
 import org.parceler.ParcelConstructor
 import org.parceler.ParcelProperty
 
 @JsonClass(generateAdapter = true)
-@Parcel
+@Parcelize
 data class ArticleOverview @ParcelConstructor constructor(
     @ParcelProperty("id") val id: String,
     @ParcelProperty("title") val title: String,
     @ParcelProperty("user") val user: User,
     @Json(name = "created_at") @ParcelProperty("createdAt") val createdAt: String,
     @ParcelProperty("tags") val tags: List<Tag>
-) {
+) : Parcelable {
 
     @JsonClass(generateAdapter = true)
-    @Parcel
+    @Parcelize
     data class User @ParcelConstructor constructor(
         @ParcelProperty("id") val id: String
-    )
+    ) : Parcelable
 
     @JsonClass(generateAdapter = true)
-    @Parcel
+    @Parcelize
     data class Tag @ParcelConstructor constructor(
         @ParcelProperty("name") val name: String
-    )
+    ) : Parcelable
 }
