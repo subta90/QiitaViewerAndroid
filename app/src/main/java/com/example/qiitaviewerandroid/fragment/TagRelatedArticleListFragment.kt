a88a02759e6aa38b6f3f0d22f46fddc2e1a24844
subta90 <the.bird.nobility298@gmail.com>
2020-11-01 15:52:08 +0900

    Show tag related articles without header contents

--- app/src/main/java/com/example/qiitaviewerandroid/fragment/TagRelatedArticleListFragment.kt
+++ app/src/main/java/com/example/qiitaviewerandroid/fragment/TagRelatedArticleListFragment.kt
@@ -7,8 +7,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
-import com.example.qiitaviewerandroid.R
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.navArgs
+import com.example.qiitaviewerandroid.databinding.FragmentTagRelatedArticleListBinding
 import com.example.qiitaviewerandroid.view.articlelist.ArticleListItemAdapter
+import com.example.qiitaviewerandroid.view.common.TagsAdapter
+import com.example.qiitaviewerandroid.view.tagrelatedarticlelist.TagRelatedArticleListItemAdapter
 import com.example.qiitaviewerandroid.view.tagrelatedarticlelist.TagRelatedArticleListViewModel
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.collectLatest
@@ -16,10 +20,33 @@ import kotlinx.coroutines.launch
 
 class TagRelatedArticleListFragment : Fragment() {
 
+    private val args: TagRelatedArticleListFragmentArgs by navArgs()
+
     private val viewModel: TagRelatedArticleListViewModel by lazy {
         ViewModelProvider.NewInstanceFactory().create(TagRelatedArticleListViewModel::class.java)
     }
 
+    private val itemListener: ArticleListItemAdapter.ArticleListItemListner by lazy {
+        ArticleListItemAdapter.ArticleListItemListner {
+            val action =
+                ArticleListFragmentDirections.actionArticleListFragmentToArticleDetailFragment(it)
+            view?.findNavController()?.navigate(action)
+        }
+    }
+
+    private val tagListener: TagsAdapter.TagButtonItemListener by lazy {
+        TagsAdapter.TagButtonItemListener {
+            val action =
+                ArticleListFragmentDirections.actionArticleListFragmentToTagRelatedArticleListFragment(
+                    it
+                )
+            view?.findNavController()?.navigate(action)
+        }
+    }
+
+    private val adapter: TagRelatedArticleListItemAdapter by lazy {
+        TagRelatedArticleListItemAdapter(args.tag, itemListener, tagListener)
+    }
 
     private var searchJob: Job? = null
 
@@ -27,18 +54,28 @@ class TagRelatedArticleListFragment : Fragment() {
         searchJob?.cancel()
         searchJob = lifecycleScope.launch {
             viewModel.searchTagRelatedArticleList(tagID).collectLatest {
-                //adapter.submitData(it)
+                adapter.submitData(it)
             }
         }
-
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tag_related_article_list, container, false)
+        val binding = FragmentTagRelatedArticleListBinding.inflate(inflater)
+
+        binding.lifecycleOwner = viewLifecycleOwner
+
+        binding.tagRelatedArticleListSwipeRefresh.setOnRefreshListener {
+            adapter.refresh()
+        }
+
+        binding.tagRelatedArticleListRecyclerview.adapter = adapter
+
+        search(args.tag.name)
+
+        return binding.root
     }
 
 }
