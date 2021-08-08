4efaced065207c34d78f083348a97b526c772b62
subta90 <the.bird.nobility298@gmail.com>
2021-08-08 17:58:53 +0900

    取得した詳細文をTextViewに反映する処理（WIP)

--- app/src/main/java/com/example/qiitaviewerandroid/fragment/ArticleDetailFragment.kt
+++ app/src/main/java/com/example/qiitaviewerandroid/fragment/ArticleDetailFragment.kt
@@ -6,26 +6,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.navArgs
-import androidx.recyclerview.widget.RecyclerView
 import com.example.qiitaviewerandroid.R
+import com.example.qiitaviewerandroid.databinding.FragmentArticleDetailBinding
 import com.example.qiitaviewerandroid.view.articledetail.ArticleDetailViewModel
-import com.example.qiitaviewerandroid.view.articledetail.LatestArticleDetailState
 import com.example.qiitaviewerandroid.view.common.TagsAdapter
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.launch
 
 class ArticleDetailFragment : Fragment() {
 
     private val args: ArticleDetailFragmentArgs by navArgs()
 
     private val viewModel: ArticleDetailViewModel by lazy {
-        ViewModelProvider.NewInstanceFactory().create(ArticleDetailViewModel::class.java)
+        val factory = ArticleDetailViewModel.Factory(args.articleOverview.id)
+        ViewModelProvider(this, factory)[ArticleDetailViewModel::class.java]
     }
 
     private val tagListener = TagsAdapter.TagButtonItemListener {
@@ -37,7 +32,13 @@ class ArticleDetailFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_article_detail, container, false)
+        val binding = FragmentArticleDetailBinding.inflate(inflater)
+        binding.lifecycleOwner = viewLifecycleOwner
+        binding.viewModel = viewModel
+        val tagsAdapter = TagsAdapter(tagListener)
+        binding.articleDetailTagsView.adapter = tagsAdapter
+        tagsAdapter.submitList(args.articleOverview.tags)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -50,22 +51,6 @@ class ArticleDetailFragment : Fragment() {
 
         val dateView = view.findViewById<TextView>(R.id.article_detail_date_view)
         dateView.text = args.articleOverview.createdAt
-
-        val tagsAdapter = TagsAdapter(tagListener)
-        val tagsView = view.findViewById<RecyclerView>(R.id.article_detail_tags_view)
-        tagsView.adapter = tagsAdapter
-        tagsAdapter.submitList(args.articleOverview.tags)
-
-        lifecycleScope.launch {
-            repeatOnLifecycle(Lifecycle.State.STARTED) {
-                viewModel.articleDetailState.collect {
-                    when (it) {
-                        is LatestArticleDetailState.Success -> print("success")
-                        is LatestArticleDetailState.Error -> print("error")
-                    }
-                }
-            }
-        }
 
     }
 }
--- app/src/main/java/com/example/qiitaviewerandroid/view/articledetail/ArticleDetailViewModel.kt
+++ app/src/main/java/com/example/qiitaviewerandroid/view/articledetail/ArticleDetailViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.qiitaviewerandroid.view.articledetail
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
 import com.example.qiitaviewerandroid.api.ArticleDetailApiService
 import com.example.qiitaviewerandroid.model.ArticleDetail
@@ -11,8 +12,15 @@ import kotlinx.coroutines.launch
 
 class ArticleDetailViewModel(private val itemID: String): ViewModel() {
 
-    private val _articleDetailState = MutableStateFlow(LatestArticleDetailState.Success(null))
-    val articleDetailState: StateFlow<LatestArticleDetailState> = _articleDetailState
+    // TODO: Daggerを入れる前の繋ぎ
+    class Factory(private val itemID: String): ViewModelProvider.NewInstanceFactory() {
+        override fun <T: ViewModel?> create(modelClass: Class<T>): T {
+            return ArticleDetailViewModel(itemID) as T
+        }
+    }
+
+    private val _articleDetailState: MutableStateFlow<LatestArticleDetailState?> = MutableStateFlow(null)
+    val articleDetailState: StateFlow<LatestArticleDetailState?> = _articleDetailState
 
     // TODO: injection
     private val repository = ArticleDetailRepository(service = ArticleDetailApiService.create())
@@ -30,4 +38,9 @@ class ArticleDetailViewModel(private val itemID: String): ViewModel() {
 sealed class LatestArticleDetailState {
     data class Success(val articleDetail: ArticleDetail?): LatestArticleDetailState()
     data class Error(val exception: Throwable): LatestArticleDetailState()
+
+    fun toBody(): String? {
+        return if (this is Success) this.articleDetail?.body else null
+    }
 }
+
