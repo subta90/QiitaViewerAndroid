3520f2aa4c85de2ac3fc55f1307e25524469ff7e
subta90 <the.bird.nobility298@gmail.com>
2021-08-09 14:00:53 +0900

    Change to LiveData from StateFlow because databinding with StateFlow is not working.

--- app/src/main/java/com/example/qiitaviewerandroid/view/articledetail/ArticleDetailViewModel.kt
+++ app/src/main/java/com/example/qiitaviewerandroid/view/articledetail/ArticleDetailViewModel.kt
@@ -1,14 +1,12 @@
 package com.example.qiitaviewerandroid.view.articledetail
 
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
 import com.example.qiitaviewerandroid.api.ArticleDetailApiService
 import com.example.qiitaviewerandroid.model.ArticleDetail
 import com.example.qiitaviewerandroid.repository.ArticleDetailRepository
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class ArticleDetailViewModel(private val itemID: String): ViewModel() {
 
@@ -19,17 +17,19 @@ class ArticleDetailViewModel(private val itemID: String): ViewModel() {
         }
     }
 
-    private val _articleDetailState: MutableStateFlow<LatestArticleDetailState?> = MutableStateFlow(null)
-    val articleDetailState: StateFlow<LatestArticleDetailState?> = _articleDetailState
+    private val _articleDetailState: MutableLiveData<LatestArticleDetailState?> = MutableLiveData(null)
+    val articleDetailState: LiveData<LatestArticleDetailState?> = _articleDetailState
 
     // TODO: injection
     private val repository = ArticleDetailRepository(service = ArticleDetailApiService.create())
 
     init {
-        viewModelScope.launch {
+        viewModelScope.launch(Dispatchers.IO) {
             // TODO: Error Handling
             val articleDetail = repository.fetchArticleDetail(itemID)
-            _articleDetailState.value = LatestArticleDetailState.Success(articleDetail)
+            withContext(Dispatchers.Main) {
+                _articleDetailState.value = LatestArticleDetailState.Success(articleDetail)
+            }
         }
     }
 
